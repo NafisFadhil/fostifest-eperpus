56ae3ebd2da93f869384476f55a9898d731cd043
NafisFadhil <nafisfadhil28@gmail.com>
2024-10-12 06:23:39 +0700

    add default logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
 	'logo' => '<b>E - Perpus</b>',
-	'logo_img' => '/vendor/adminlte/dist/img/AdminLTELogo.png',
+	'logo_img' => '/asset/images/logo.jpg',
 	'logo_img_class' => 'brand-image img-circle elevation-3',
 	'logo_img_xl' => null,
 	'logo_img_xl_class' => 'brand-image-xs',
@@ -86,7 +86,7 @@ return [
 	'auth_logo' => [
 		'enabled' => false,
 		'img' => [
-			'path' => '/vendor/adminlte/dist/img/AdminLTELogo.png',
+			'path' => '/asset/images/logo.jpg',
 			'alt' => 'Auth Logo',
 			'class' => '',
 			'width' => 50,
@@ -113,8 +113,8 @@ return [
 		'enabled' => true,
 		'mode' => 'fullscreen',
 		'img' => [
-			'path' => '/vendor/adminlte/dist/img/AdminLTELogo.png',
-			'alt' => 'AdminLTE Preloader Image',
+			'path' => '/asset/images/logo.jpg',
+			'alt' => 'Logo',
 			'effect' => 'animation__shake',
 			'width' => 60,
 			'height' => 60,
